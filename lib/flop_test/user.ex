ac0612fc830d3ef56ffcc73f63d3c6e52e9c6b09
balysv <balys@valentukevicius.com>
2025-04-14 17:19:15 +0200

    Extra fields hack

--- lib/flop_test/user.ex
+++ lib/flop_test/user.ex
@@ -5,13 +5,23 @@ defmodule FlopTest.User do
   @derive {
     Flop.Schema,
     filterable: [],
-    sortable: [:id, :name],
+    sortable: [:id, :name, :name_c],
     default_order: %{
       order_by: [:id],
       order_directions: [:asc]
     },
     default_pagination_type: :offset,
-    pagination_types: [:first, :last, :offset]
+    pagination_types: [:first, :last, :offset],
+    # "extra_fields" hack to try force a coalesce
+    adapter_opts: [
+      join_fields: [
+        name_c: [
+          binding: :extra_fields,
+          field: :name_c,
+          ecto_type: :string
+        ]
+      ]
+    ]
   }
 
   schema "users" do
--- test/flop_test/user_test.exs
+++ test/flop_test/user_test.exs
@@ -21,7 +21,6 @@ defmodule FlopTest.UserTest do
         for: User
       )
 
-
     # Results: [{"a", 3}, {"b", 4}, {"c", 5}, {"d", 6}, {nil, 7}]
     assert Enum.map(results, & &1.name) == ["a", "b", "c", "d", nil]
 
@@ -29,6 +28,44 @@ defmodule FlopTest.UserTest do
       Flop.validate_and_run(
         query,
         %{first: 5, after: meta.end_cursor, order_by: [:name, :id], order_directions: [:asc]},
+        for: User
+      )
+
+    # Results: [{"a", 3}, {"b", 4}, {"c", 5}, {"d", 6}, {nil, 7}]
+    # but instead it should be: [{nil, 7}, {nil, 8}]
+    assert Enum.map(results, & &1.name) == [nil, nil]
+  end
+
+  test "extra fields hack for a page ending in `nil`" do
+    extra_fields_query =
+      from(u1 in User,
+        where: u1.id == parent_as(:user).id,
+        select: %{
+          name_c: fragment("COALESCE(?, 'zzzzzzzzzzzzzz')", u1.name)
+        }
+      )
+
+    query =
+      from(u in User, as: :user)
+      |> join(:inner_lateral, [user: u], f in subquery(extra_fields_query),
+        on: true,
+        as: :extra_fields
+      )
+
+    {:ok, {results, meta}} =
+      Flop.validate_and_run(
+        query,
+        %{first: 5, order_by: [:name_c, :id], order_directions: [:asc]},
+        for: User
+      )
+
+    # Results: [{"a", 3}, {"b", 4}, {"c", 5}, {"d", 6}, {nil, 7}]
+    assert Enum.map(results, & &1.name) == ["a", "b", "c", "d", nil]
+
+    {:ok, {results, meta}} =
+      Flop.validate_and_run(
+        query,
+        %{first: 5, after: meta.end_cursor, order_by: [:name_c, :id], order_directions: [:asc]},
         for: User
       )
 
